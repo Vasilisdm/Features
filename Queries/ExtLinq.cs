a26abbee108de89d2673d149c16477a354cc3f2a
vasilisdimitriou <vasilisdmr@gmail.com>
2020-01-26 16:58:02 +0200

    Implemented my version of Where's Linq method, Filter.

---
+++ Queries/ExtLinq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queries
+{
+    public static class ExtLinq
+    {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            List<T> result = new List<T>();
+
+            foreach(var item in source)
+            {
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+    }
+}
