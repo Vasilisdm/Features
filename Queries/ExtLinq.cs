0e071c8eeec7d43c7a2ab96ee1ef9b76e5ba9f19
vasilisdimitriou <vasilisdmr@gmail.com>
2020-01-26 17:19:15 +0200

    Used yield keyword in order to defer the execution and proceed to the next item in the movie list on when the previous one has finished execution on the progam class.

--- Queries/ExtLinq.cs
+++ Queries/ExtLinq.cs
@@ -7,17 +7,13 @@ namespace Queries
     {
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
-            List<T> result = new List<T>();
-
             foreach(var item in source)
             {
                 if (predicate(item))
                 {
-                    result.Add(item);
+                    yield return item;
                 }
             }
-
-            return result;
         }
     }
 }
