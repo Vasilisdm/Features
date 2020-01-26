910ee39b340a1d937a8c35de4170eea430e057cf
vasilisdimitriou <vasilisdmr@gmail.com>
2020-01-26 20:14:47 +0200

    Wrote the query of the fuel csv into a query syntax instead of a method one.

---
+++ Cars/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            //return
+            //    File.ReadAllLines(path)
+            //    .Skip(1)
+            //    .Where(line => line.Length > 1)
+            //    .Select(Car.ParseFromCsv)
+            //    .ToList();
+
+            var query = from line in File.ReadAllLines(path).Skip(1)
+                        where (line.Length > 1)
+                        select (Car.ParseFromCsv(line));
+
+            return query.ToList();
+        }
+    }
+}
