c2909dcbbdb1a01d79462dff8494c0a039573792
vasilisdimitriou <vasilisdmr@gmail.com>
2020-01-28 21:52:12 +0200

    Removed unecessary static methods from program along with the unused using statements.

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Cars;
-using CarExtensions;
 
 namespace CarExt
 {
@@ -50,39 +47,6 @@ namespace CarExt
             {
                 Console.WriteLine(manufacturer.Name);
             }
-        }
-
-        private static List<Manufacturer> ProcessManufacturers(string path)
-        {
-            var query = File.ReadAllLines(path)
-                            .Where(line => line.Length > 1)
-                            .Select(line =>
-                                {
-                                    var manufacturerFileColumns = line.Split(",");
-                                    return new Manufacturer
-                                    {
-                                        Name = manufacturerFileColumns[0],
-                                        HeadQuarters = manufacturerFileColumns[1],
-                                        Year = int.Parse(manufacturerFileColumns[2])
-                                    };
-                                });
-
-            return query.ToList();
-        }
-
-        private static List<Car> ProcessCars(string path)
-        {
-            
-            var query = File.ReadAllLines(path)
-                            .Skip(1)
-                            .Where(line => line.Length > 1)
-                            .ToCar();
-
-            //var query = from line in File.ReadAllLines(path).Skip(1)
-            //            where (line.Length > 1)
-            //            select (Car.ParseFromCsv(line));
-
-            return query.ToList();
         }
     }
 }
