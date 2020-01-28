51239d188ae59dd4a0fc3a1b250dfb2d2676838d
vasilisdimitriou <vasilisdmr@gmail.com>
2020-01-28 20:44:24 +0200

    Used the ToCar extension method on the method syntax query of the process file method.

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Cars;
+using CarExtensions;
 
-namespace Cars
+namespace CarExt
 {
     class Program
     {
@@ -11,38 +13,43 @@ namespace Cars
         {
             var cars = ProcessFile("fuel.csv");
 
-            var combinedEf = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+            var efficientBmwMethodSyntax = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
                                  .OrderByDescending(c => c.Combined)
-                                 .ThenBy(c => c.Name).First();
+                                 .ThenBy(c => c.Name)
+                                 .First();
 
-            Console.WriteLine($"Most efficient BMW found in method syntax: {combinedEf.Name}");
+            Console.WriteLine($"Most efficient BMW found in method syntax: {efficientBmwMethodSyntax.Name}");
 
-            var combinedEfQuerySyntax = (from car in cars
-                                        where car.Manufacturer == "BMW" && car.Year == 2016
-                                        orderby car.Combined descending, car.Name ascending
-                                        select car).First();
+            var efficientBmwQuerySyntax = (from car in cars
+                                           where car.Manufacturer == "BMW" && car.Year == 2016
+                                           orderby car.Combined descending, car.Name ascending
+                                           select car).First();
 
-            Console.WriteLine($"Most efficient BMW found in query syntax: {combinedEfQuerySyntax.Name}");
+            Console.WriteLine($"Most efficient BMW found in query syntax: {efficientBmwQuerySyntax.Name}");
 
-            //Console.WriteLine("Most fuel efficient cars");
-            //foreach (Car car in combinedEf)
-            //{
-            //    Console.WriteLine($"{car.Name} : {car.Combined}");
-            //}
+
+            var topTeCarsCombinedEf = cars.OrderByDescending(c => c.Combined)
+                                          .ThenBy(c => c.Name)
+                                          .Take(10)
+                                          .ToList();
+
+            foreach (Car car in topTeCarsCombinedEf)
+            {
+                Console.WriteLine($"{car.Name} : {car.Combined}");
+            }
         }
 
         private static List<Car> ProcessFile(string path)
         {
-            //return
-            //    File.ReadAllLines(path)
-            //    .Skip(1)
-            //    .Where(line => line.Length > 1)
-            //    .Select(Car.ParseFromCsv)
-            //    .ToList();
+            
+            var query = File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(line => line.Length > 1)
+                    .ToCar();
 
-            var query = from line in File.ReadAllLines(path).Skip(1)
-                        where (line.Length > 1)
-                        select (Car.ParseFromCsv(line));
+            //var query = from line in File.ReadAllLines(path).Skip(1)
+            //            where (line.Length > 1)
+            //            select (Car.ParseFromCsv(line));
 
             return query.ToList();
         }
