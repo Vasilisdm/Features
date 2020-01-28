e2c411557cd6ef311b2c53c5679190650dd70521
vasilisdimitriou <vasilisdmr@gmail.com>
2020-01-28 21:35:00 +0200

    Added manufacturer class with the necessary properties.

---
+++ Cars/Manufacturer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Cars
+{
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string HeadQuarters { get; set; }
+        public int Year { get; set; }
+    }
+}
