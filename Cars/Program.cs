7cc6be065cb9635bd41c185b7a11a7b658d8f4b2
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-08 15:40:35 +0200

    removed unecessary console writelines and added explanatory comments.

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -11,51 +11,39 @@ namespace CarExt
             var cars = FileProcess.Cars("fuel.csv");
             var manufacturers = FileProcess.Manufacturers("manufacturers.csv");
 
-            //var topTenEfficientCarsM = cars.Join(manufacturers,
-            //                                c => new { c.Manufacturer, c.Year},
-            //                                m => new { Manufacturer = m.Name, m.Year },
-            //                                (c, m) => new
-            //                                {
-            //                                    m.HeadQuarters,
-            //                                    c.Name,
-            //                                    c.Combined
-            //                                })
-            //                                .OrderByDescending(c => c.Combined)
-            //                                .ThenBy(c => c.Name)
-            //                                .Take(10);
+            // Join in method extension syntax
+            var topTenEfficientCarsM = cars.Join(manufacturers,
+                                            c => new { c.Manufacturer, c.Year },
+                                            m => new { Manufacturer = m.Name, m.Year },
+                                            (c, m) => new
+                                            {
+                                                m.HeadQuarters,
+                                                c.Name,
+                                                c.Combined
+                                            })
+                                            .OrderByDescending(c => c.Combined)
+                                            .ThenBy(c => c.Name)
+                                            .Take(10);
+            // Join in query syntax
+            var topTenEfficientCars = (from car in cars
+                                       join manufacturer in manufacturers
+                                       on new { car.Manufacturer, car.Year } equals new { Manufacturer = manufacturer.Name, manufacturer.Year }
+                                       orderby car.Combined descending, car.Name ascending
+                                       select new
+                                       {
+                                           manufacturer.HeadQuarters,
+                                           car.Name,
+                                           car.Combined
+                                       }).Take(10);
 
-            //foreach (var car in topTenEfficientCarsM)
-            //{
-            //    Console.WriteLine($"Efficient Cars {car.HeadQuarters} : {car.Name} : {car.Combined}");
-            //}
-
-
-            //var topTenEfficientCars = (from car in cars
-            //                           join manufacturer in manufacturers
-            //                           on new {car.Manufacturer, car.Year} equals new { Manufacturer = manufacturer.Name, manufacturer.Year }
-            //                               orderby car.Combined descending, car.Name ascending
-            //                               select new
-            //                               {
-            //                                   manufacturer.HeadQuarters,
-            //                                   car.Name,
-            //                                   car.Combined
-            //                               }).Take(10);
-
+            // Group join usage
             var efficientCarsGroupedByManufacturerM = manufacturers.GroupJoin(cars, m => m.Name, c => c.Manufacturer, (m, g) => new
             {
                 Manufacturer = m,
                 Cars = g
             }).GroupBy(m => m.Manufacturer.HeadQuarters);
 
-            //foreach (var group in efficientCarsGroupedByManufacturerM)
-            //{
-            //    Console.WriteLine($"{group.Key}");
-            //    foreach (var car in group.SelectMany(g => g.Cars).OrderByDescending(c => c.Combined).Take(3))
-            //    {
-            //        Console.WriteLine($"\t{car.Name} : {car.Combined}");
-            //    }
-            //}
-
+            // Usage of join and putting the result into a variable in order to group by Headquarters.
             var efficientCarsGroupedByManufacturer = from manufacturer in manufacturers
                                                      join car in cars on manufacturer.Name equals car.Manufacturer into carGroup
                                                      select new
@@ -65,6 +53,7 @@ namespace CarExt
                                                      } into result
                                                      group result by result.Manufacturer.HeadQuarters;
 
+            // Use an anonymous type to return the min,max and avg consumption grouped by Manufacturer and ordered by MaxConsumption
             var efficiencyByManufacturer = from car in cars
                                            group car by car.Manufacturer into carGroup
                                            select new
