d9ab8df4d4b8361ca47c802158a1b686106a8ede
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-08 16:28:04 +0200

    Created carStatistics class with the necessary properties and the Accumulate method.

---
+++ Cars/CarStatistics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Cars
+{
+    public class CarStatistics
+    {
+        public CarStatistics()
+        {
+            Max = Int32.MinValue;
+            Min = Int32.MaxValue;
+        }
+
+        public CarStatistics Accumulate(Car car)
+        {
+            Count += 1;
+            Total += car.Combined;
+            Max = Math.Max(Max, car.Combined);
+            Min = Math.Min(Min, car.Combined);
+
+            return this;
+        }
+
+        private int Min { get; set; }
+        private int Max { get; set; }
+        private double Average { get; set; }
+        private int Count { get; set; }
+        private int Total { get; set; }
+    }
+}
