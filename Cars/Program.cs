8cd92794888e343e7d7d40b3ead028a40ceed547
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-08 17:38:26 +0200

    Created cars.xml

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -11,7 +11,23 @@ namespace CarExt
         {
             var carRecords = FileProcess.Cars("fuel.csv");
 
-            
+            var document = new XDocument();
+            var cars = new XElement("Cars");
+
+            foreach (var record in carRecords)
+            {
+                var car = new XElement("Car");
+                var name = new XElement("Name", record.Name);
+                var combined = new XElement("Combined", record.Combined);
+
+                car.Add(name);
+                car.Add(combined);
+
+                cars.Add(car);
+            }
+
+            document.Add(cars);
+            document.Save("Cars.xml");
         }
     }
 }
